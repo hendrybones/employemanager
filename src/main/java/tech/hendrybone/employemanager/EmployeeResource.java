04c39b6576a81138a58f0cf7623699d0e193d701
hendrybones <hendrymwamburi56@gmail.com>
2021-09-13 20:40:53 +0300

    adding controller

--- src/main/java/tech/hendrybone/employemanager/EmployeeResource.java
+++ src/main/java/tech/hendrybone/employemanager/EmployeeResource.java
@@ -1,4 +1,27 @@
 package tech.hendrybone.employemanager;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.hendrybone.employemanager.model.Employee;
+import tech.hendrybone.employemanager.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+
 public class EmployeeResource {
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Employee>>getAllEmployee(){
+        List<Employee> employees=employeeService.findAllEmployee();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
 }
